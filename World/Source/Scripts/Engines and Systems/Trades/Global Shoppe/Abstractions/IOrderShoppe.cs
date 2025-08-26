ef16629a8c0c92d80b93cbd57449c04e128b409b
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-08-26 08:51:49 -0400

    -

--- World/Source/Scripts/Engines and Systems/Trades/Global Shoppe/Abstractions/IOrderShoppe.cs
+++ World/Source/Scripts/Engines and Systems/Trades/Global Shoppe/Abstractions/IOrderShoppe.cs
@@ -1,15 +1,15 @@
 namespace Server.Engines.GlobalShoppe
 {
-    public interface IOrderShoppe
-    {
-        void CompleteOrder(IOrderContext order, Mobile from, TradeSkillContext context, RewardType selectedReward);
+	public interface IOrderShoppe
+	{
+		void CompleteOrder(IOrderContext order, Mobile from, TradeSkillContext context, RewardType selectedReward);
 
-        string GetDescription(IOrderContext order);
+		string GetDescription(IOrderContext order);
 
-        void OpenOrderGump(int index, Mobile from, TradeSkillContext context);
+		void OpenOrderGump(int index, Mobile from, TradeSkillContext context);
 
-        void OpenRewardSelectionGump(int index, Mobile from, TradeSkillContext context);
+		void OpenRewardSelectionGump(int index, Mobile from, TradeSkillContext context);
 
-        void RejectOrder(int index, TradeSkillContext context);
-    }
+		void RejectOrder(int index, TradeSkillContext context);
+	}
 }
