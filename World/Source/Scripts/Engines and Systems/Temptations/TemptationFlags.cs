94ab336b752d180fe19458a434edbc7c7f5ceb29
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-09-17 19:44:04 -0400

    -

--- World/Source/Scripts/Engines and Systems/Temptations/TemptationFlags.cs
+++ World/Source/Scripts/Engines and Systems/Temptations/TemptationFlags.cs
@@ -8,9 +8,9 @@ namespace Server.Temptation
 		None							= 0x00000000,
 		Puzzle_master					= 0x00000001,
 		Strongest_Avenger				= 0x00000002,
-		Famine							= 0x00000004,
+		Famine							= 0x00000004, // Incomplete
 		I_can_take_it					= 0x00000008,
-		This_is_just_a_tribute			= 0x00000010,
+		This_is_just_a_tribute			= 0x00000010, // Incomplete
 		Deathwish						= 0x00000020,
 	}
 }
