70aa9db809c6d7e5d75a842848bf9cd0fce05d09
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-09-30 08:58:46 -0400

    - Add message logger

---
+++ World/Source/Scripts/Engines and Systems/Messaging/Icons.cs
@@ -0,0 +1,14 @@
+namespace Server.Engines.Messaging
+{
+	public class Icons
+	{
+		public const string Imp = ":imp:";
+		public const string Jeans = ":jeans:";
+		public const string Jigsaw = ":jigsaw:";
+		public const string Pepe_Heart = "<:pepe_heart:1400873130322563205>";
+		public const string Pepe_Yes = "<:pepe_yes:1400872876646600885>";
+		public const string Question = ":question:";
+		public const string Skull_Crossbones = ":skull_crossbones:";
+		public const string Stew = ":stew:";
+	}
+}
---
+++ World/Source/Scripts/Engines and Systems/Messaging/MessageLogger.cs
@@ -0,0 +1,156 @@
+using Server.Misc;
+using Server.Mobiles;
+using Server.Temptation;
+using System.Linq;
+using System.Text;
+
+namespace Server.Engines.Messaging
+{
+	public class MessageLogger
+	{
+		public static string ChatWebhookUrl;
+		public static string EventWebhookUrl;
+
+		private static MessageLogger m_Engine;
+		private NotificationService ChatService;
+		private NotificationService EventService;
+
+		public static MessageLogger Instance
+		{
+			get
+			{
+				if (m_Engine == null)
+					m_Engine = new MessageLogger();
+
+				return m_Engine;
+			}
+		}
+
+		public static void Initialize()
+		{
+			if (!string.IsNullOrWhiteSpace(ChatWebhookUrl))
+			{
+				Instance.ChatService = NotificationService.Start(ChatWebhookUrl);
+
+				CustomEventSink.ChatMessage += new ChatMessageEventHandler(Instance.OnChatMessage);
+			}
+
+			if (!string.IsNullOrWhiteSpace(EventWebhookUrl))
+			{
+				Instance.EventService = NotificationService.Start(EventWebhookUrl);
+
+				EventSink.Login += new LoginEventHandler(Instance.OnPlayerLogin);
+				EventSink.Logout += new LogoutEventHandler(Instance.OnPlayerLogout);
+				EventSink.PlayerDeath += new PlayerDeathEventHandler(Instance.OnDeath);
+				CustomEventSink.LootPull += new LootPullEventHandler(Instance.OnLootPull);
+				CustomEventSink.EventLogged += new EventLoggedHandler(Instance.OnEventLogged);
+				CustomEventSink.BeginJourney += new BeginJourneyHandler(Instance.OnBeginJourney);
+			}
+		}
+
+		private void OnBeginJourney(BeginJourneyArgs e)
+		{
+			var player = e.Mobile;
+			var message = string.Format("*{0}* has begun their journey", player.Name);
+
+			var temptationFlags = player.Temptations.Flags;
+			if (temptationFlags != TemptationFlags.None)
+			{
+				var temptations = new string[]
+				{
+					temptationFlags.HasFlag(TemptationFlags.Puzzle_master) ? Icons.Jigsaw: null,
+					temptationFlags.HasFlag(TemptationFlags.Strongest_Avenger) ? Icons.Jeans : null,
+					temptationFlags.HasFlag(TemptationFlags.Famine) ? Icons.Stew : null,
+					temptationFlags.HasFlag(TemptationFlags.I_can_take_it) ?Icons.Imp : null,
+					// temptationFlags.HasFlag(TemptationFlags.This_is_just_a_tribute) ? Icons.Question  : null,
+					temptationFlags.HasFlag(TemptationFlags.Deathwish) ? Icons.Skull_Crossbones : null,
+				};
+				message += string.Format(".\r\n{0} They have been tempted: {1}", Icons.Pepe_Yes, string.Join(" ", temptations.Where(x => x != null)));
+			}
+
+			EventService.QueueMessage(message);
+		}
+
+		private void OnChatMessage(ChatMessageEventArgs e)
+		{
+			var player = e.Mobile as PlayerMobile;
+			if (player == null) return;
+			if (!player.PublicInfo) return;
+
+			ChatService.QueueMessage(string.Format("`{0}:` {1}", player.Name, e.Message));
+		}
+
+		private void OnDeath(PlayerDeathEventArgs args)
+		{
+			var player = args.Mobile as PlayerMobile;
+			if (player == null) return;
+			if (AccessLevel.Counselor < player.AccessLevel) return;
+			if (!player.PublicInfo) return;
+
+			var builder = new StringBuilder();
+
+			var killer = player.LastKiller;
+			if (killer == player)
+				builder.AppendFormat("*{0}* killed themselves", player.Name);
+			else if (killer == null)
+				builder.AppendFormat("*{0}* was killed", player.Name);
+			else
+				builder.AppendFormat("*{0}* was killed by *{1}*", player.Name, killer.Name);
+
+			if (player.Temptations.HasPermanentDeath)
+			{
+				builder.AppendFormat(" after *{0}*. Rest in peace {1}",
+					player.GameTime.TotalDays < 1
+						? string.Format("{0}h {1}m", player.GameTime.Hours, player.GameTime.Minutes)
+						: string.Format("{0}d {1}h {2}m", player.GameTime.Days, player.GameTime.Hours, player.GameTime.Minutes),
+					Icons.Pepe_Heart
+				);
+			}
+			else
+				builder.Append(" !");
+
+			EventService.QueueMessage(builder.ToString());
+		}
+
+		private void OnEventLogged(EventLoggedArgs e)
+		{
+			if (!e.Mobile.PublicInfo) return;
+
+			string @event = e.Event;
+			if (!e.IsAnonymous)
+				@event = string.Format("*{0}* {1}", e.Mobile.Name, @event.Trim());
+
+			EventService.QueueMessage(@event);
+		}
+
+		private void OnLootPull(LootPullEventArgs e)
+		{
+			var player = e.Mobile as PlayerMobile;
+			if (player == null) return;
+			if (e.Item == null) return;
+			if (AccessLevel.Counselor < player.AccessLevel) return;
+
+			EventService.QueueMessage(string.Format("*{0}* has acquired *{1}*!", player.RawName, e.Item.Name));
+		}
+
+		private void OnPlayerLogin(LoginEventArgs args)
+		{
+			var player = args.Mobile as PlayerMobile;
+			if (player == null) return;
+			if (AccessLevel.Counselor < player.AccessLevel) return;
+			if (!player.PublicInfo) return;
+
+			EventService.QueueMessage(string.Format("*{0} {1}* has entered the realm", player.Name, player.Title != null ? player.Title : "the " + GetPlayerInfo.GetSkillTitle(player)));
+		}
+
+		private void OnPlayerLogout(LogoutEventArgs args)
+		{
+			var player = args.Mobile as PlayerMobile;
+			if (player == null) return;
+			if (AccessLevel.Counselor < player.AccessLevel) return;
+			if (!player.PublicInfo) return;
+
+			EventService.QueueMessage(string.Format("*{0}* has left the realm", player.RawName));
+		}
+	}
+}
