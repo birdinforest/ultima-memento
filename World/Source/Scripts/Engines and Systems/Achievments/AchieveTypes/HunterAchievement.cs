c430b3acfa009b8f81773423c7e952ef1a5fec47
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-10-01 08:51:38 -0400

    - Expose target type

--- World/Source/Scripts/Engines and Systems/Achievments/AchieveTypes/HunterAchievement.cs
+++ World/Source/Scripts/Engines and Systems/Achievments/AchieveTypes/HunterAchievement.cs
@@ -6,19 +6,19 @@ namespace Scripts.Mythik.Systems.Achievements
 {
     public class HunterAchievement : BaseAchievement
     {
-        private readonly Type m_Mobile;
+        public readonly Type EnemyType;
 
         public HunterAchievement(int id, int catid, int itemIcon, bool hiddenTillComplete, BaseAchievement prereq, int total, string title, string desc, short rewardPoints, Type targets, params Type[] rewards)
             : base(id, catid, itemIcon, hiddenTillComplete, prereq, title, desc, rewardPoints, total, rewards)
         {
-            m_Mobile = targets;
+            EnemyType = targets;
             EventSink.OnKilledBy += EventSink_OnKilledBy;
         }
 
         private void EventSink_OnKilledBy(OnKilledByArgs e)
         {
             var player = e.KilledBy as PlayerMobile;
-            if (player != null && e.Killed.GetType() == m_Mobile)
+            if (player != null && e.Killed.GetType() == EnemyType)
             {
                 AchievementSystem.SetAchievementStatus(player, this, 1);
             }
