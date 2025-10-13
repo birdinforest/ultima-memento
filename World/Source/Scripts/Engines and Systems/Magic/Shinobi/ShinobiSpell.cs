090e86594f2d8bca09f14b06b5b9fdb97be482b3
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-10-13 18:01:07 -0400

    - Shinobi spells no longer get double benefit from LMC - Shinobi spells no longer require double mana to cast

--- World/Source/Scripts/Engines and Systems/Magic/Shinobi/ShinobiSpell.cs
+++ World/Source/Scripts/Engines and Systems/Magic/Shinobi/ShinobiSpell.cs
@@ -84,12 +84,7 @@ namespace Server.Spells.Shinobi
 
 			Caster.TithingPoints -= requiredTithing;
 
-			if ( !base.CheckFizzle() )
-				return false;
-
-			Caster.Mana -= mana;
-
-			return true;
+			return base.CheckFizzle();
 		}
 
 		public override void SayMantra()
@@ -115,7 +110,7 @@ namespace Server.Spells.Shinobi
 
 		public override int GetMana()
 		{
-			return ScaleMana( RequiredMana );
+			return RequiredMana;
 		}
 
 		public override void OnDisturb( DisturbType type, bool message )
