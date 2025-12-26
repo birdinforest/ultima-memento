5216ca47840d8b4e81819fba38e2103a8f226820
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-12-26 11:56:28 -0500

    - Add ability to toggle avatar and shop

---
+++ World/Source/Scripts/Engines and Systems/Avatar/AvatarCommand.cs
@@ -0,0 +1,66 @@
+using Server.Commands;
+using Server.Gumps;
+using Server.Misc;
+using Server.Mobiles;
+
+namespace Server.Engines.Avatar
+{
+	public class AvatarCommand
+	{
+		public static void Initialize()
+		{
+			CommandSystem.Register("avatar-enable", AccessLevel.Player, new CommandEventHandler(EnableAvatarCommand));
+			CommandSystem.Register("avatar-shop", AccessLevel.Player, new CommandEventHandler(OpenAvatarShopCommand));
+		}
+
+		[Usage("avatar-enable")]
+		[Description("Enables the Avatar status for the Player.")]
+		public static void EnableAvatarCommand(CommandEventArgs e)
+		{
+			var from = (PlayerMobile)e.Mobile;
+			if (!InGypsyEncampment(from))
+			{
+				from.SendMessage("You must be in the Gypsy encampment to enable the Avatar status.");
+				return;
+			}
+
+			if (from.Avatar.Active)
+			{
+				from.SendMessage("You already have the Avatar status enabled.");
+				return;
+			}
+
+			var confirmation = new ConfirmationGump(
+				from,
+				"Enable Avatar Status?",
+				"Are you sure you wish to enable the Avatar status? This will reset your character and allow you to use the Avatar features.",
+				() =>
+				{
+					var _ = AvatarEngine.Instance.GetOrCreateContext(from);
+					from.SendMessage("You have enabled the Avatar status.");
+					CharacterCreation.ResetCharacter(from);
+				}
+			);
+			from.SendGump(confirmation);
+		}
+
+		[Usage("avatar-shop")]
+		[Description("Opens the Avatar Shop for the Player.")]
+		public static void OpenAvatarShopCommand(CommandEventArgs e)
+		{
+			var from = (PlayerMobile)e.Mobile;
+			if (!InGypsyEncampment(from))
+			{
+				from.SendMessage("You must be in the Gypsy encampment to open the Avatar Shop.");
+				return;
+			}
+
+			from.SendGump(new AvatarShopGump(from));
+		}
+
+		private static bool InGypsyEncampment(PlayerMobile from)
+		{
+			return from.Region != null && from.Region.Name == "the Forest";
+		}
+	}
+}
