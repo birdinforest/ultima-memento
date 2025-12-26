4d7c98071e4d714928055349c2dcdbc345731ea2
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-12-26 13:36:53 -0500

    - Add command to expose current balance

--- World/Source/Scripts/Engines and Systems/Avatar/AvatarCommand.cs
+++ World/Source/Scripts/Engines and Systems/Avatar/AvatarCommand.cs
@@ -11,6 +11,18 @@ namespace Server.Engines.Avatar
 		{
 			CommandSystem.Register("avatar-enable", AccessLevel.Player, new CommandEventHandler(EnableAvatarCommand));
 			CommandSystem.Register("avatar-shop", AccessLevel.Player, new CommandEventHandler(OpenAvatarShopCommand));
+			CommandSystem.Register("avatar-balance", AccessLevel.Player, args =>
+			{
+				var from = (PlayerMobile)args.Mobile;
+				if (!from.Avatar.Active) return;
+
+				var warChest = from.Avatar.PointsSaved;
+				var earned = from.Avatar.PointsFarmed;
+				var total = warChest + earned;
+				from.SendMessage("You have '{0:n0}' coins in your war chest.", warChest);
+				from.SendMessage("You have earned '{0:n0}' coins.", earned);
+				from.SendMessage("You will have a total of '{0:n0}' coins to spend in the Gypsy encampment.", total);
+			});
 			CommandSystem.Register("avatar-points", AccessLevel.Player, args =>
 			{ // TODO: Remove this!
 				var from = (PlayerMobile)args.Mobile;
