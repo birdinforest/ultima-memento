14d25178d7af0766b4b0e1bab93efa0b7f654765
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-01-03 14:07:47 -0500

    - Remove [Avatar-Balance command

--- World/Source/Scripts/Engines and Systems/Avatar/AvatarCommand.cs
+++ World/Source/Scripts/Engines and Systems/Avatar/AvatarCommand.cs
@@ -12,18 +12,6 @@ namespace Server.Engines.Avatar
 			CommandSystem.Register("avatar-enable", AccessLevel.Player, new CommandEventHandler(EnableAvatarCommand));
 			CommandSystem.Register("avatar-force-enable", AccessLevel.Player, new CommandEventHandler(ForceEnableAvatarCommand));
 			CommandSystem.Register("avatar-shop", AccessLevel.Player, new CommandEventHandler(OpenAvatarShopCommand));
-			CommandSystem.Register("avatar-balance", AccessLevel.Player, args =>
-			{
-				var from = (PlayerMobile)args.Mobile;
-				if (!from.Avatar.Active) return;
-
-				var treasury = from.Avatar.PointsSaved;
-				var earned = from.Avatar.PointsFarmed;
-				var total = treasury + earned;
-				from.SendMessage("You have '{0:n0}' coins in your treasury.", treasury);
-				from.SendMessage("You have earned '{0:n0}' coins.", earned);
-				from.SendMessage("You will have a total of '{0:n0}' coins to spend in the Gypsy encampment.", total);
-			});
 		}
 
 		[Usage("avatar-enable")]
