e7cafdd2b39cfebbbb2d908bc588a5d8715ef766
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-01-06 08:28:43 -0500

    - Minor refactor for clarity

--- World/Source/Scripts/Engines and Systems/Quests/Fishing/FishingQuestFunctions.cs
+++ World/Source/Scripts/Engines and Systems/Quests/Fishing/FishingQuestFunctions.cs
@@ -139,9 +139,7 @@ namespace Server.Misc
 		{
 			PlayerMobile PC = (PlayerMobile)m;
 
-			int PirateHunt = 0;
-			int WorldPick = Utility.RandomMinMax( 0, 14 );
-				if ( Utility.RandomMinMax( 1, 8 ) == 1 && 4000 < fee ){ PirateHunt = 1; WorldPick = Utility.RandomMinMax( 0, 6 ); }
+			var PirateHunt = Utility.RandomMinMax( 1, 8 ) == 1 && 4000 < fee;
 
 			var options = new List<Land>
 			{
@@ -171,7 +169,7 @@ namespace Server.Misc
 			{
 				reg = Region.Find( target.Location, target.Map );
 
-				if ( PirateHunt == 1 )
+				if ( PirateHunt )
 				{
 					if ( searchLocation == Land.Sosaria )
 					{
