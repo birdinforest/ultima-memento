2235e2d01c0041ef47a25de73209059652e27ac2
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-01-18 06:57:46 -0500

    - Ghost Ship Anchor now weighs 80, down from 200

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -32,6 +32,7 @@ namespace Server.Misc
 			builder.Append("- Item - The book of jokes now has a description<br>");
 			builder.Append("- Item - Reduced Gladiator's Collar stats and increased strength requirement<br>");
 			builder.Append("- Item - [Organize containers now allow you to place items in them<br>");
+			builder.Append("- Item - Ghost Ship Anchor now weighs 80, down from 200<br>");
 			builder.Append("- Misc - Boat movement is no longer blocked by corpses<br>");
 			builder.Append("- Misc - Corpses can no longer be looted while it would cause a criminal action<br>");
 			builder.Append("- Misc - Combat is now prevented in starting zones and Jail<br>");
