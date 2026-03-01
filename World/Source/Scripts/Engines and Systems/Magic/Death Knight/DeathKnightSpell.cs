5e11f42d3fbc86fe8c5c34941a6a0652dc4f26fb
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-03-01 16:34:20 -0500

    -

--- World/Source/Scripts/Engines and Systems/Magic/Death Knight/DeathKnightSpell.cs
+++ World/Source/Scripts/Engines and Systems/Magic/Death Knight/DeathKnightSpell.cs
@@ -1,11 +1,6 @@
 using System;
-using Server;
-using Server.Spells;
 using Server.Network;
-using Server.Mobiles;
 using Server.Items;
-using System.Collections.Generic;
-using System.Collections;
 
 namespace Server.Spells.DeathKnight
 {
@@ -224,7 +219,6 @@ namespace Server.Spells.DeathKnight
 		{
 			int souls = 0;
 
-			ArrayList targets = new ArrayList();
 			foreach ( Item item in World.Items.Values )
 			{
 				if ( item is SoulLantern )
