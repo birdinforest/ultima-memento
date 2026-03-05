e1847bd5bf10dd24cf530bdb4ae283d54ab1c4b1
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-03-05 08:25:43 -0500

    - Format/cleanup on bard songs

--- World/Source/Scripts/Engines and Systems/Magic/Bard/Spells/IceCarolSong.cs
+++ World/Source/Scripts/Engines and Systems/Magic/Bard/Spells/IceCarolSong.cs
@@ -1,12 +1,6 @@
 using System;
 using System.Collections;
-using Server;
 using Server.Mobiles;
-using Server.Network;
-using Server.Items;
-using Server.Targeting;
-using Server.Gumps;
-using Server.Spells;
 using Server.Misc;
 
 namespace Server.Spells.Song
@@ -18,58 +12,58 @@ namespace Server.Spells.Song
 				-1
 			);
 
-		public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds( 5 ); } }
-		public override double RequiredSkill{ get{ return 50.0; } }
-		public override int RequiredMana{ get{ return 12; } }
-		
-		public IceCarolSong( Mobile caster, Item scroll) : base( caster, scroll, m_Info )
+		public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds(5); } }
+		public override double RequiredSkill { get { return 50.0; } }
+		public override int RequiredMana { get { return 12; } }
+
+		public IceCarolSong(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
 		{
 		}
 
-        public override void OnCast()
-        {
+		public override void OnCast()
+		{
 			base.OnCast();
 
 			bool sings = false;
- 
-			if( CheckSequence() )
+
+			if (CheckSequence())
 			{
 				sings = true;
- 
+
 				ArrayList targets = new ArrayList();
 
-				foreach ( Mobile m in Caster.GetMobilesInRange( 10 ) )
+				foreach (Mobile m in Caster.GetMobilesInRange(10))
 				{
-					if ( isFriendly( Caster, m ) && m.ColdResistance < MySettings.S_MaxResistance )
-						targets.Add( m );
+					if (isFriendly(Caster, m) && m.ColdResistance < MySettings.S_MaxResistance)
+						targets.Add(m);
 				}
 
-				for ( int i = 0; i < targets.Count; ++i )
+				for (int i = 0; i < targets.Count; ++i)
 				{
 					Mobile m = (Mobile)targets[i];
-					
-					TimeSpan duration = TimeSpan.FromSeconds( (double)(MusicSkill( Caster ) * 2) ); 
-                    int amount = MyServerSettings.PlayerLevelMod( (int)(MusicSkill( Caster ) / 16), Caster );
 
-					if ( ( amount + m.ColdResistance ) > MySettings.S_MaxResistance )
+					TimeSpan duration = TimeSpan.FromSeconds((double)(MusicSkill(Caster) * 2));
+					int amount = MyServerSettings.PlayerLevelMod((int)(MusicSkill(Caster) / 16), Caster);
+
+					if ((amount + m.ColdResistance) > MySettings.S_MaxResistance)
 						amount = MySettings.S_MaxResistance - m.ColdResistance;
-	
-					m.SendMessage( "Your resistance to cold has increased." );
-					ResistanceMod mod1 = new ResistanceMod( ResistanceType.Cold, + amount );
-						
-					m.AddResistanceMod( mod1 );
-						
-					m.FixedParticles( 0x373A, 10, 15, 5012, 0x480, 3, EffectLayer.Waist );
-						
-					new ExpireTimer( m, mod1, duration ).Start();
+
+					m.SendMessage("Your resistance to cold has increased.");
+					ResistanceMod mod1 = new ResistanceMod(ResistanceType.Cold, +amount);
+
+					m.AddResistanceMod(mod1);
+
+					m.FixedParticles(0x373A, 10, 15, 5012, 0x480, 3, EffectLayer.Waist);
+
+					new ExpireTimer(m, mod1, duration).Start();
 
 					string args = String.Format("{0}", amount);
-					BuffInfo.RemoveBuff( m, BuffIcon.IceCarol );
-					BuffInfo.AddBuff( m, new BuffInfo( BuffIcon.IceCarol, 1063573, 1063574, duration, m, args.ToString(), true));
+					BuffInfo.RemoveBuff(m, BuffIcon.IceCarol);
+					BuffInfo.AddBuff(m, new BuffInfo(BuffIcon.IceCarol, 1063573, 1063574, duration, m, args.ToString(), true));
 				}
 			}
 
-			BardFunctions.UseBardInstrument( m_Book.Instrument, sings, Caster );
+			BardFunctions.UseBardInstrument(m_Book.Instrument, sings, Caster);
 			FinishSequence();
 		}
 
@@ -78,7 +72,7 @@ namespace Server.Spells.Song
 			private Mobile m_Mobile;
 			private ResistanceMod m_Mods;
 
-			public ExpireTimer( Mobile m, ResistanceMod mod, TimeSpan delay ) : base( delay )
+			public ExpireTimer(Mobile m, ResistanceMod mod, TimeSpan delay) : base(delay)
 			{
 				m_Mobile = m;
 				m_Mods = mod;
@@ -87,16 +81,16 @@ namespace Server.Spells.Song
 			public void DoExpire()
 			{
 				PlayerMobile dpm = m_Mobile as PlayerMobile;
-				m_Mobile.RemoveResistanceMod( m_Mods );
-				
+				m_Mobile.RemoveResistanceMod(m_Mods);
+
 				Stop();
 			}
 
 			protected override void OnTick()
 			{
-				if ( m_Mobile != null )
+				if (m_Mobile != null)
 				{
-					m_Mobile.SendMessage( "The effect of the ice carol wears off." );
+					m_Mobile.SendMessage("The effect of the ice carol wears off.");
 					DoExpire();
 				}
 			}
--- World/Source/Scripts/Engines and Systems/Magic/Bard/Spells/MagicFinaleSong.cs
+++ World/Source/Scripts/Engines and Systems/Magic/Bard/Spells/MagicFinaleSong.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Collections;
-using Server;
 using Server.Mobiles;
-using Server.Network;
 using Server.Items;
-using Server.Targeting;
-using Server.Gumps;
-using Server.Spells;
 using Server.Misc;
 
 namespace Server.Spells.Song
@@ -18,52 +13,52 @@ namespace Server.Spells.Song
 				-1
 			);
 
-		public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds( 5 ); } }
-		public override double RequiredSkill{ get{ return 90.0; } }
-		public override int RequiredMana{ get{ return 35; } }
-		
-		public MagicFinaleSong( Mobile caster, Item scroll) : base( caster, scroll, m_Info )
+		public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds(5); } }
+		public override double RequiredSkill { get { return 90.0; } }
+		public override int RequiredMana { get { return 35; } }
+
+		public MagicFinaleSong(Mobile caster, Item scroll) : base(caster, scroll, m_Info)
 		{
 		}
 
-        public override void OnCast()
-        {
+		public override void OnCast()
+		{
 			base.OnCast();
 
 			bool sings = false;
- 
-			if( CheckSequence() )
+
+			if (CheckSequence())
 			{
 				sings = true;
- 
+
 				ArrayList targets = new ArrayList();
 
-				foreach ( Mobile m in Caster.GetMobilesInRange( 4 ) )
+				foreach (Mobile m in Caster.GetMobilesInRange(4))
 				{
-					if ( m is BaseCreature )
+					if (m is BaseCreature)
 					{
 						BaseCreature mn = m as BaseCreature;
-						if ( mn.IsTempEnemy )
-							targets.Add( m );
+						if (mn.IsTempEnemy)
+							targets.Add(m);
 					}
 
-					if ( m is BaseCreature && ((BaseCreature)m).Summoned )
-						targets.Add( m );
+					if (m is BaseCreature && ((BaseCreature)m).Summoned)
+						targets.Add(m);
 				}
-				
-				Caster.FixedParticles( 0x3709, 1, 30, 9965, 5, 7, EffectLayer.Waist );
-				
-				for ( int i = 0; i < targets.Count; ++i )
+
+				Caster.FixedParticles(0x3709, 1, 30, 9965, 5, 7, EffectLayer.Waist);
+
+				for (int i = 0; i < targets.Count; ++i)
 				{
 					Mobile m = (Mobile)targets[i];
-					
-					Effects.SendLocationParticles( EffectItem.Create( m.Location, m.Map, EffectItem.DefaultDuration ), 0x3728, 8, 20, 5042 );
+
+					Effects.SendLocationParticles(EffectItem.Create(m.Location, m.Map, EffectItem.DefaultDuration), 0x3728, 8, 20, 5042);
 
 					m.Delete();
 				}
 			}
 
-			BardFunctions.UseBardInstrument( m_Book.Instrument, sings, Caster );
+			BardFunctions.UseBardInstrument(m_Book.Instrument, sings, Caster);
 			FinishSequence();
 		}
 	}
