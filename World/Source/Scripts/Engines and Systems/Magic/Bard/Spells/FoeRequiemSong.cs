6bc719a40ba1f60d97337f0b5cc683634f8010b4
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-03-08 10:28:04 -0400

    - Increase damage by 50% - Reduce damage based on range - Do true damage instead of 20% of each element - Do damage over time - Use equipped instrument

--- World/Source/Scripts/Engines and Systems/Magic/Bard/Spells/FoeRequiemSong.cs
+++ World/Source/Scripts/Engines and Systems/Magic/Bard/Spells/FoeRequiemSong.cs
@@ -1,15 +1,18 @@
-using System;
-using Server.Targeting;
-using Server.Mobiles;
 using Server.Items;
 using Server.Misc;
+using Server.Mobiles;
+using Server.Targeting;
+using System;
+using System.Collections;
 
 namespace Server.Spells.Song
 {
 	public class FoeRequiemSong : Song
 	{
+		private static readonly Hashtable m_Table = new Hashtable();
+
 		private static SpellInfo m_Info = new SpellInfo(
-				"Foe Requiem", "*plays an foe requiem*",
+				"Foe Requiem", "*plays a foe requiem*",
 				-1
 			);
 
@@ -18,14 +21,43 @@ namespace Server.Spells.Song
 		}
 
 		public override TimeSpan CastDelayBase { get { return TimeSpan.FromSeconds(2); } }
-		public override double RequiredSkill { get { return 80.0; } }
 		public override int RequiredMana { get { return 30; } }
+		public override double RequiredSkill { get { return 80.0; } }
+		public override bool UseDefaultInstrument { get { return false; } }
 
-		public override void OnCast()
+		public static bool HasEffect(Mobile m)
 		{
-			base.OnCast();
+			return m_Table[m] != null;
+		}
 
-			Caster.Target = new InternalTarget(this);
+		public static void RemoveEffect(Mobile m, Mobile messageRecipient = null)
+		{
+			Timer t = (Timer)m_Table[m];
+
+			if (t != null)
+			{
+				t.Stop();
+				m_Table.Remove(m);
+
+				if (messageRecipient != null)
+				{
+					messageRecipient.SendMessage("The effect of {0} wears off.", m_Info.Name);
+				}
+			}
+		}
+
+		public override bool CheckCast()
+		{
+			if (!base.CheckCast())
+				return false;
+
+			if (GetEquippedInstrument() == null)
+			{
+				Caster.SendMessage("You must equip an instrument for this song!");
+				return false;
+			}
+
+			return true;
 		}
 
 		public virtual bool CheckSlayer(BaseInstrument instrument, Mobile defender)
@@ -39,13 +71,21 @@ namespace Server.Spells.Song
 			return false;
 		}
 
+		public override void OnCast()
+		{
+			base.OnCast();
+
+			Caster.Target = new InternalTarget(this);
+		}
+
 		public void Target(Mobile m)
 		{
-			Spellbook book = Spellbook.Find(Caster, -1, SpellbookType.Song);
-			if (book == null)
+			var instrument = GetEquippedInstrument();
+			if (instrument == null)
+			{
+				Caster.SendMessage("You must equip an instrument for this song!");
 				return;
-
-			m_Book = (SongBook)book;
+			}
 
 			if (m == null)
 			{
@@ -63,37 +103,57 @@ namespace Server.Spells.Song
 				}
 				else if (CheckHSequence(m))
 				{
-					sings = true;
+					// Each caster can only affect one target at a time.
+					if (HasEffect(Caster))
+					{
+						RemoveEffect(Caster);
+					}
 
-					Mobile source = Caster;
+					sings = true;
 
 					SpellHelper.Turn(Caster, m);
 
-					bool IsSlayer = false;
-					if (m is BaseCreature) { IsSlayer = CheckSlayer(m_Book.Instrument, m); }
+					double damage = MusicSkill(Caster) / 10;
+					if (m is BaseCreature && CheckSlayer(instrument, m)) damage *= 2;
 
-					double damage = (double)(MusicSkill(Caster) / 15);
-
-					if (IsSlayer)
+					var i = 0;
+					m_Table[Caster] = Timer.DelayCall(TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(2), () =>
 					{
-						damage = damage * 2;
-					}
+						const int MAX_RANGE = 10;
+						const int MAX_TICKS = 5;
+						if (m == null || m.Deleted || !m.Alive || MAX_TICKS < ++i || !m.InRange(Caster, MAX_RANGE))
+						{
+							RemoveEffect(m, Caster);
+							return;
+						}
+
+						var currentDamage = damage;
+						if (!m.InRange(Caster, 3))
+							currentDamage *= 0.50;
+						else if (!m.InRange(Caster, 2))
+							currentDamage *= 0.75;
+
+						m.Damage((int)currentDamage, Caster);
+					});
 
 					m.FixedParticles(0x374A, 10, 15, 5028, EffectLayer.Head);
-					source.MovingParticles(m, 0x379F, 7, 0, false, true, 3043, 4043, 0x211);
+					Caster.MovingParticles(m, 0x379F, 7, 0, false, true, 3043, 4043, 0x211);
 					m.PlaySound(0x1EA);
-
-					SpellHelper.Damage(this, m, damage, 20, 20, 20, 20, 20);
 				}
 
-				BardFunctions.UseBardInstrument(m_Book.Instrument, sings, Caster);
+				BardFunctions.UseBardInstrument(instrument, sings, Caster);
 				FinishSequence();
 			}
 		}
 
+		private BaseInstrument GetEquippedInstrument()
+		{
+			return Caster.Trinket as BaseInstrument;
+		}
+
 		private class InternalTarget : Target
 		{
-			private FoeRequiemSong m_Owner;
+			private readonly FoeRequiemSong m_Owner;
 
 			public InternalTarget(FoeRequiemSong owner) : base(12, false, TargetFlags.Harmful)
 			{
