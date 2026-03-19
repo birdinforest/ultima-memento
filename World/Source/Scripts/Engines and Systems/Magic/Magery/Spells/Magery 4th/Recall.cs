4ebe30cd8355933d6857e03ca541fe4f803d0701
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-03-19 15:48:07 -0400

    - Fast Travel and Gate Travel mechanisms now ignore Mobiles at destination tile

--- World/Source/Scripts/Engines and Systems/Magic/Magery/Spells/Magery 4th/Recall.cs
+++ World/Source/Scripts/Engines and Systems/Magic/Magery/Spells/Magery 4th/Recall.cs
@@ -89,7 +89,7 @@ namespace Server.Spells.Fourth
 			{
 				Caster.SendLocalizedMessage( 502359, "", 0x22 ); // Thou art too encumbered to move.
 			}
-			else if ( !map.CanSpawnMobile( loc.X, loc.Y, loc.Z ) )
+			else if ( !map.CanSpawnMobile( loc.X, loc.Y, loc.Z, false ) )
 			{
 				Caster.SendLocalizedMessage( 501942 ); // That location is blocked.
 			}
--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -40,6 +40,7 @@ namespace Server.Misc
 			builder.Append("- Misc - Treasure hoards now announce the items that are pulled<br>");
 			builder.Append("- Spell - Curse now reduces elemental resistances for Monsters by 10<br>");
 			builder.Append("- Spell - Shadow Jump now allows targeting mobiles<br>");
+			builder.Append("- Spell - Fast Travel and Gate Travel mechanisms now ignore Mobiles at destination tile<br>");
 			builder.Append("- Skill - Tracking bonus for Ninjitsu now builds twice as fast<br>");
 			builder.Append("- Skill - Tracking is now 50% more likely to gain<br>");
 			builder.Append("- Skill - Death Strike now applies a mortal wound to the target<br>");
