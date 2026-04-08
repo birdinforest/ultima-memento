1b00594a0de7891defd22e99bd2641e586833e5b
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-04-08 08:58:14 -0400

    - Make song book match the skill requirement

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -24,6 +24,7 @@ namespace Server.Misc
 			builder.Append("<br>");
 			builder.Append("Fixes<br>");
 			builder.Append("- Gump - Fix issue where Chemist Bar (Alchemist Pouch) buttons didn't work<br>");
+			builder.Append("- Gump - Foe Requiem in song book matches the skill requirement (50 instead of 80)<br>");
 			builder.Append("- Spell - Syth and Jedi spells now properly consume the displayed crystal cost, down from 200% or 300%<br>");
 			builder.Append("- Spell - Insult implementation and displayed info now match<br>");
 
