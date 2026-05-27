868c66949867c591b439f9931eeedc809a1d0160
Devin Ulrey <Devin.Ulrey@gmail.com>
2026-05-27 13:41:20 -0400

    - Prevent stacking if resource mismatch

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -50,6 +50,7 @@ namespace Server.Misc
 			builder.Append("- Gump - Fix images for bee hives<br>");
 			builder.Append("- Gump - Fix issue where runic scribe tools could not craft spellbooks<br>");
 			builder.Append("- Item - Items that can block equipping now use the proper method<br>");
+			builder.Append("- Item - Fix issue where Transmutation potions could mix<br>");
 			builder.Append("- Spell - Syth and Jedi spells now properly consume the displayed crystal cost, down from 200% or 300%<br>");
 			builder.Append("- Spell - Insult implementation and displayed info now match<br>");
 
