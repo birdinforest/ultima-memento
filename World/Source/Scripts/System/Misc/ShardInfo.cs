6832898e2c157e09f8fb992753b5371db66ed01f
Derek Wang <birdinforest@gmail.com>
2026-07-17 09:39:42 +1000

    chore: pump to 3.0.10

--- World/Source/Scripts/System/Misc/ShardInfo.cs
+++ World/Source/Scripts/System/Misc/ShardInfo.cs
@@ -8,7 +8,7 @@ namespace Server.Misc
 		/// Single source of truth for the shard/game version.
 		/// Update this when releasing a new version.
 		/// </summary>
-		public static readonly Version CurrentVersion = new Version(3, 0, 9);
+		public static readonly Version CurrentVersion = new Version(3, 0, 10);
 
 		/// <summary>
 		/// Display string: "Version: X.Y.Z"
